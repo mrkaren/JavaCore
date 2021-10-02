82fa09b99896c2da4c5dbb9aca63e2e85dfd071d
Karen Gasparyan <mrkaren92@gmail.com>
2021-10-02 12:03:39 +0400

    add IntExample, change ForTest

--- src/group1/lesson2/ForTest.java
+++ src/group1/lesson2/ForTest.java
@@ -2,6 +2,8 @@ package group1.lesson2;
 
 public class ForTest {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
---
+++ src/group1/lesson3/IntExample.java
@@ -0,0 +1,9 @@
+package group1.lesson3;
+
+public class IntExample {
+
+    public static void main(String[] args) {
+        int a = 70;
+        System.out.println(a);
+    }
+}
